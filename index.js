f9019ed11921163b41a146d17bdcc6fd6ac5fe64
Kanchan Heera <161306339+kanchanheera@users.noreply.github.com>
2024-03-25 03:52:57 +0500

    compiled to js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import inquirer from "inquirer";
 // 1) Computer will generate a random number - Done
 // 2) User input for guessing numbers - Done
-// 3) Compare user input with computer generated number & show result
+// 3) Compare user input with computer generated number & show result - Done
 const randomNumber = Math.floor(Math.random() * 6 + 1);
 const answers = await inquirer.prompt([
     {
